7068675770cc6055de37d34e086831f959590f9e
Yossi Ben Harosh <reshetech@users.noreply.github.com>
2016-05-04 12:18:36 +0300

    Update factory-utilis.js

--- factory-utilis.js
+++ factory-utilis.js
@@ -1,15 +1,14 @@
 app.factory('utilis', function() {
-	var obj = {};
-	
+        var obj = {};
 	
 	obj.indexof = function(array, item) {
-		var index = -1;
-		var count = array.length;
-		for(var i=0; i<count; i++){
-			if(item == array[i]) index = i;
-		}
+	    var index = -1;
+	    var count = array.length;
+	    for(var i=0; i<count; i++){
+	        if(item == array[i]) index = i;
+	    }
 		
-		return index;
+	    return index;
 	}
 		
 	obj.inArray = function(array, value) {
@@ -17,9 +16,17 @@ app.factory('utilis', function() {
 	}
 		
 	obj.removeArrayItem = function(array, item) { 
-		var index = obj.indexof(array, item);
+	    var index = obj.indexof(array, item);
 	    return array.splice(index, 1);     
-    }
+        }
+	
+	obj.firstToUpper = function(str) {
+            return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+	}
+	
+	obj.getHash = function() {
+	    return window.location.hash.substr(1);
+	}
 	
 	
 	return obj;
