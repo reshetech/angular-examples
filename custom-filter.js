6987b5bbe6da2e878f7178634226dcfa645b23f1
Yossi Ben Harosh <reshetech@users.noreply.github.com>
2016-05-04 17:14:54 +0300

    Update custom-filter.js

--- custom-filter.js
+++ custom-filter.js
@@ -1,6 +1,28 @@
-app.filter('ilDateFormat', function myDateFormat($filter){
+// Format the time string into European date format.
+app.filter('ilDateFormat', function ($filter){
   return function(text){
     var  tempdate= new Date(text.replace(/-/g,"/"));
     return $filter('date')(tempdate, "dd-MM-yyyy");
   }
+});
+
+
+// Get items from array that have the propertyId.
+app.filter('byPropertyType', function() {
+
+    return function(res,propertyTypeId) {
+        var out = [];
+		
+		if(propertyTypeId == null || typeof propertyTypeId == "undefined") return res;
+		
+	    var count = res.length;
+	    for(var i=0; i<count;i++) {
+		    if(res[i].property_type == propertyTypeId) {
+				out.push(res[i]);
+			}
+        }
+	  
+        return out;
+    }
+
 });
